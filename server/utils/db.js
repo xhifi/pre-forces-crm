ed7d93cabdb3519358dc1a3c50939bcc5347be29
shifi <shifa.newversion@gmail.com>
2023-08-15 13:08:00 +0500

    database connection for node-pg pooler

---
+++ server/utils/db.js
@@ -0,0 +1,13 @@
+const Pool = require("pg").Pool;
+
+const db = new Pool({
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+});
+
+module.exports = db;
+
+// SQL Transactions
