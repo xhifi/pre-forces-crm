af47b0726dedf9314bad05c97e3835da9ff18eff
shifi <shifa.newversion@gmail.com>
2023-08-15 13:12:42 +0500

    Correctly implemented and tested custom errors to eliminate repeated usage of res.send in error situations

--- server/controllers/students/getOne.js
+++ server/controllers/students/getOne.js
@@ -1,3 +1,26 @@
+const db = require("../../utils/pg");
+const BadRequestError = require("../../errors/BadRequestError");
+const { StatusCodes } = require("http-status-codes");
+
 module.exports = async (req, res) => {
-  res.send({ msg: "Get One Controller Accessed" });
+  const { params: id } = req;
+  // Sanitize the input
+  if (!id) {
+    throw new BadRequestError(
+      "No ID sent along request, please provide ID as a request parameter"
+    );
+  }
+  // check in db if doesnt exist send notfound error
+  const userFound = await db.query(`SELECT * FROM students WHERE id = $1`, [
+    id,
+  ]);
+
+  if (userFound.rowCount === 0) {
+    throw new NotFoundError(`No user with ${id} found in system`);
+  }
+
+  // if user is found, send the data back to the user in a response i.e. user information from the db
+  userData = userFound.rows[0];
+
+  res.status(StatusCodes.ACCEPTED).send({ msg: userData });
 };
