05ef71ac4c6578eaa57c84f41342db630536219f
shifi <shifa.newversion@gmail.com>
2023-08-15 13:11:07 +0500

    Main index.js file fixed to accomodate newly created 404 error and error boundary middlewares

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(require("cors")());
 
-app.use("/api/alpha/s", require("./routes/studentsRoute"));
+app.use("/api/alpha/s", require("./routes/studentsRoute")); // Students routes
+
+app.use(require("./middlewares/ErrorHandler.js")); //Error Boundary Middleware
+app.use(require("./middlewares/FourOhFour")); //404 Middleware
 
 require("./utils/initServer")(app, process.env.APPLICATION_PORT);
